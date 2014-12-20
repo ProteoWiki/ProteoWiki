70d19b0f3f1646c83ed02cb77666ac2c21a63042
Toni Hermoso Pulido <toniher@cau.cat>
2014-12-20 12:57:51 +0100

    trim

--- mediawiki/extensions/ProteoWiki/includes/ProteoWiki_ParserFunctions.php
+++ mediawiki/extensions/ProteoWiki/includes/ProteoWiki_ParserFunctions.php
@@ -14,8 +14,10 @@ class ProteoWikiParserFunctions {
 		if ( !empty( $input ) ) {
 			global $wgOut;
 			$wgOut->addModules( 'ext.ProteoWiki' );
+			
+			$input = trim( strip_tags( $input ) );
 
-			$output = "<div class='proteowikiconf' data-delimiter='".$delimiter."' data-separator='".$separator."'>".trim( $input )."</div>";
+			$output = "<div class='proteowikiconf' data-delimiter='".$delimiter."' data-separator='".$separator."'>".$input."</div>";
 		}
 
 		return array( $output, 'noparse' => true, 'isHTML' => true );
