bf27c6d6c1411ec713936a3a9492a096143021a7
toniher <toniher@cau.cat>
2015-02-06 16:35:03 +0100

    rm for now

--- mediawiki/extensions/ProteoWiki/ProteoWiki.i18n.magic.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$magicWords = array();
-
-/** English (English) */
-$magicWords['en'] = array(
-	'proteowikiconf' => array( 0, 'proteowikiconf' ),
-	'proteowikiformlinks' => array( 0, 'proteowikiformlinks' )
-);
