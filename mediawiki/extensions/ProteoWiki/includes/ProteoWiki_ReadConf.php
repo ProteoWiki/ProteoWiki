f7823519a11083b5a806b2f26a630f84968af229
Toni Hermoso Pulido <toniher@cau.cat>
2015-02-11 00:28:47 +0100

    read

---
+++ mediawiki/extensions/ProteoWiki/includes/ProteoWiki_ReadConf.php
@@ -0,0 +1,15 @@
+<?php
+
+/** Here a set of reading configuration functions **/
+
+
+class ProteoWikiReadConf {
+
+    /** read Configuration and return value */
+    public static function readConf( $class="" ) {
+        
+        // READ all CSV from wgProteoWikiPages
+        
+    }
+    
+}
